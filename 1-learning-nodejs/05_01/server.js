f4ee6f3f3ce12ae2013d53b7abe2f3a17fae0667
Mehmet Tarhan <hi@memtarhan.com>
2022-05-01 16:41:12 +0100

    [Learning NodeJS] - Create a post messages service

--- 1-learning-nodejs/05_01/server.js
+++ 1-learning-nodejs/05_01/server.js
@@ -1,10 +1,13 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 /// - Create an app
 var app = express();
 
 /// - To use static html file
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var messages = [
   { name: "Jim", message: "Hey Dwight!" },
@@ -13,6 +16,12 @@ var messages = [
 
 app.get("/messages", (request, response) => {
   response.send(messages);
+});
+
+app.post("/messages", (request, response) => {
+  console.log("POST: /messages -- ", request.body);
+  messages.push(request.body);
+  response.sendStatus(200);
 });
 
 /// - Listen to the app
