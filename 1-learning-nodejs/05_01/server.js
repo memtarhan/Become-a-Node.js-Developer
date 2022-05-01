d2b06616936d18e1ba80b192d5bd1ea58b6b05b2
Mehmet Tarhan <hi@memtarhan.com>
2022-05-01 15:55:01 +0100

    [Learning NodeJS] - Create a get messages service

--- 1-learning-nodejs/05_01/server.js
+++ 1-learning-nodejs/05_01/server.js
@@ -3,7 +3,17 @@ var express = require("express");
 /// - Create an app
 var app = express();
 
+/// - To use static html file
 app.use(express.static(__dirname));
+
+var messages = [
+  { name: "Jim", message: "Hey Dwight!" },
+  { name: "Dwight", message: "Michael!" },
+];
+
+app.get("/messages", (request, response) => {
+  response.send(messages);
+});
 
 /// - Listen to the app
 var server = app.listen(3000, () => {
