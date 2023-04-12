b08d8c0b174eb7d330a45347aa9ec036610ec2ca
Mehmet Tarhan <hi@memtarhan.com>
2023-04-12 18:32:30 +0100

    [NodeJS Essential Training] - Using standard output

---
+++ 2-nodejs-essential-training/02_05/global.js
@@ -0,0 +1,32 @@
+// Using standard output
+
+process.stdout.write("Hello \n\n");
+
+const questions = [
+  "What is your name?",
+  "What would you rather be doing?",
+  "What is your preferred programming language?",
+];
+
+const answers = [];
+
+function ask(i = 0) {
+  process.stdout.write("\n\n\n" + questions[i]);
+  process.stdout.write(" > ");
+}
+
+ask();
+
+process.stdin.on("data", function (data) {
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+process.on("exit", function () {
+  process.stdout.write("\n\n\n\n");
+  process.stdout.write("Finished");
+});
