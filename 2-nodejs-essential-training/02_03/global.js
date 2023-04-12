1a6c260118bae44be4a4f2f5dea154d3d0001a69
Mehmet Tarhan <hi@memtarhan.com>
2023-04-12 15:45:58 +0100

    [NodeJS Essential Training] - Handling argument variables with process.argv

---
+++ 2-nodejs-essential-training/02_03/global.js
@@ -0,0 +1,14 @@
+// Handling argument variables with process.argv
+
+// console.log(process.argv)
+
+function grab(flag) {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1
+    return process.argv[indexAfterFlag]
+}
+
+let greeting = grab("--greeting")
+let user = grab("--user")
+
+console.log(greeting)
+console.log(user)
